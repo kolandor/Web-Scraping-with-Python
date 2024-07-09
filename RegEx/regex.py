d9e97980e45612659c3fd0b42bb9038229597e1a
Alexey Anipchenko <pyelya@gmail.com>
2024-07-09 14:40:19 +0100

    Add docks reference to regex

--- RegEx/regex.py
+++ RegEx/regex.py
@@ -1,6 +1,9 @@
 # A RegEx, or Regular Expression, is a sequence of characters that forms a search pattern.
 # RegEx can be used to check if a string contains the specified search pattern.
 
+# Dock
+# https://www.w3schools.com/python/python_regex.asp
+
 # Python has a built-in package called re, which can be used to work with Regular Expressions
 # Import the re module:
 import re
