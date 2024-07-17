08379fedff320ca04e29d16c575e96a30652fe40
Alexey Anipchenko <pyelya@gmail.com>
2024-07-17 16:00:22 +0100

    Fix BeautifulSoup remove duplication AND add improvements

--- Beautiful Soup/BeautifulSoup.py
+++ Beautiful Soup/BeautifulSoup.py
@@ -51,14 +51,12 @@ def get_html_by_url(url, saveFolder = "Beautiful Soup/htmlbin", hardload = False
 
 def get_bbc_sport_news_by_soup(newsMaxCountReturn):
     try:
-        htmlContent = get_html_by_url(r"https://www.bbc.com/sport")
+        htmlContent = get_html_by_url(r"https://www.bbc.com/sport", hardload=True)
         
         soup = BeautifulSoup(htmlContent, features="lxml")
         
-        #Get sports news promos by tag div contains data-testid="promo"
-        promos = soup.find_all("div", {"data-testid": "promo"})
-        #Filtering promos without type
-        promos = [promo for promo in promos if promo.get("type")]
+        #Get sports news promos by tag div contains data-testid="promo" AND type
+        promos = soup.find_all("div", {"data-testid": "promo", "type": True})
         
         promosData = []
         
@@ -66,39 +64,48 @@ def get_bbc_sport_news_by_soup(newsMaxCountReturn):
         
         for promo in promos:
             tempDictPromosData = {}
-            tempDictPromosData["Topics"] = []
+            try:
+                promoATags = promo.find_all("a")
+                
+                if promoATags is not None and len(promoATags):
+                    
+                    aTag = promoATags[0]
+                    
+                    #Check reference
+                    if aTag.parent.has_attr("spacing"):
+                        # The tag does not contain a full link, but navigation within the site, so we use concatenation
+                        tempDictPromosData["Link"] = f"https://www.bbc.com{aTag["href"]}"
+                        
+                        #Skip life and video (is no contains topics)
+                        if "live" in tempDictPromosData["Link"] or "videos" in tempDictPromosData["Link"]:
+                            continue
+                    else:
+                        continue
+                    
+                    #Then jump into news
+                    currentNewsHtml = get_html_by_url(tempDictPromosData["Link"], hardload=True)
+                    
+                    currentNewsSoup = BeautifulSoup(currentNewsHtml, features="lxml")
+                    
+                    #Find list of topics 
+                    ulOfTopics = currentNewsSoup.find("ul", {"spacing": "2"})
+                    if ulOfTopics is None:
+                        tempDictPromosData["Topics"] = None
+                    else:
+                        tempDictPromosData["Topics"] = []
+                    
+                        #Check parrent is a TopicList
+                        classTopicList = ulOfTopics.parent.previousSibling.get("class")
+                        if classTopicList:
+                            if "TopicList" in classTopicList[0]:
+                                for liTopic in ulOfTopics.children:
+                                    tempDictPromosData["Topics"].append(liTopic.find("a").text)
+                
+                promosData.append(tempDictPromosData)
+                counter -= 1
+            except Exception as e:
+                print(f"ERROR: {e}")
             
-            promoATags = promo.find_all("a")
-            
-            for aTag in  promoATags:
-                
-                if aTag.parent.has_attr("spacing"):
-                    # The tag does not contain a full link, but navigation within the site, so we use concatenation
-                    tempDictPromosData["Link"] = f"https://www.bbc.com{aTag["href"]}"
-                    continue
-                
-                #Main topic on th current page
-                # if aTag.parent.parent.previousSibling.text == "Attribution":
-                #     tempDictPromosData["Topics"].append(aTag.find("span").text)
-                
-                #Then jump into news
-                currentNewsHtml = get_html_by_url(tempDictPromosData["Link"])
-                
-                currentNewsSoup = BeautifulSoup(currentNewsHtml, features="lxml")
-                
-                #Find list of topics 
-                ulOfTopics = currentNewsSoup.find("ul", {"spacing": "2"})
-                
-                #Check parrent is a TopicList
-                classTopicList = ulOfTopics.parent.previousSibling.get('class')
-                if classTopicList:
-                    if "TopicList" in classTopicList[0]:
-                        for liTopic in ulOfTopics.children:
-                            tempDictPromosData["Topics"].append(liTopic.find("a").text)
-            
-            promosData.append(tempDictPromosData)
-            
-            counter -= 1
             if not counter:
                 break
         
@@ -114,7 +121,7 @@ def save_list_to_JSON_file(listResult = [], fileToSave = "result.json"):
         print()
         if fileToSave:
             with open(fileToSave, "wt", encoding="utf-8") as file:
-                    json.dump(listResult, file)
+                    json.dump(listResult, file, indent=4)
                     print(f"Written to JSON file: {fileToSave}")
     except Exception as e:
         print(f"ERROR: {e}")
